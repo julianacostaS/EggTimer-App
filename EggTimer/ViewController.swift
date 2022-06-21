3c62bddbd719b9269abba77ae7f0e29225ee0369
Juliana Costa <julianacosta_s@hotmail.com>
2022-06-21 21:21:04 +1000

    Added alarm sound and progress view bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,22 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 12]
-   
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-   
-        let hardness = sender.currentTitle
+       
         
-        if hardness == "Soft" {
-            print(eggTimes["Soft"]!)
-        } else if hardness == "Medium" {
-            print(eggTimes["Medium"]!)
+        timer.invalidate() // cancel the timer every time the button is pressed
+        let hardness = sender.currentTitle! // Soft, Medium , Hard
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    //objc = comes from objective-C. Selectors are from objective-C
+    @objc func updateTimer() {
+        //example functionality
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            //print(Float(secondsPassed) / Float(totalTime))
+           
         } else {
-            print(eggTimes["Hard"]!)
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 }
